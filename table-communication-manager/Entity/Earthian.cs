8ae98659a49b1ac27ff48c8047a2e783b611f753
tech13 <sir.code.badger@gmail.com>
2018-04-10 05:15:46 +0000

    add table communication project for create, write and read

---
+++ table-communication-manager/Entity/Earthian.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace table_communication_manager.Entity
+{
+    class Earthian : TableEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Earthian() {}
+
+        public Earthian(string name, string email)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.PartitionKey = "Earth";
+            this.RowKey = email;
+        }
+    }
+}
---
+++ table-communication-manager/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using table_communication_manager.Entity;
+
+namespace table_communication_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnection"));
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            CloudTable table = tableClient.GetTableReference("aliens");
+
+            table.CreateIfNotExists();
+
+            //CreateAlien(table, new AlienEarth("badger", "badger@localhost.earth"));
+
+            //GetAlien(table, "Earth", "badger@localhost.earth");
+
+            GetAllAliens(table);
+
+            Console.ReadKey();
+        }
+
+        static void CreateAlien(CloudTable table, Earthian earthian)
+        {
+            TableOperation insert = TableOperation.Insert(earthian);
+
+            table.Execute(insert);
+        }
+
+        static void GetAlien(CloudTable table, string partitionKey, string rowKey)
+        {
+            TableOperation retrieve = TableOperation.Retrieve<Earthian>(partitionKey, rowKey);
+
+            var result = table.Execute(retrieve);
+
+            Console.WriteLine(((Earthian)result.Result).Name);
+        }
+
+        static void GetAllAliens(CloudTable table)
+        {
+            TableQuery<Earthian> query = new TableQuery<Earthian>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Earth"));
+
+            foreach (Earthian earthian in table.ExecuteQuery(query))
+            {
+                Console.WriteLine(earthian.Name);
+            }
+        }
+    }
+}
